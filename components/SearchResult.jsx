213330922719fb4aacafa3489ca0ed58d767ceb9
Towkir7970 <towkir.aust.110@gmail.com>
2024-03-27 04:22:56 +0200

    fix errors

--- components/SearchResult.jsx
+++ components/SearchResult.jsx
@@ -6,7 +6,7 @@ export default function SearchResult({ results, term, closeSearchResults }) {
       <div className="absolute left-0 top-12 z-[999] w-full rounded-md bg-white p-4 shadow">
         <p className="!text-lg">
           Showing Results for
-          <span className="font-semibold">"{term}":</span>
+          <span className="font-semibold">{term}:</span>
         </p>
         <ul role="list" className="divide-y divide-gray-100 [&>*]:py-2">
           {results.map((result) => (
